2a71e23c8950bedc00e6cc501169d1cf3ca4ccdb
Darshan Naik <rdarshannaik2503@gmail.com>
2026-05-21 00:41:25 +0530

    Initial enterprise backend setup

---
+++ src/main/java/com/divyasphere/DivyaspherePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.divyasphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivyaspherePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivyaspherePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divyasphere/DivyaspherePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyasphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivyaspherePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
