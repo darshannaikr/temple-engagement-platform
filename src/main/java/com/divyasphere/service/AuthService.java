8a5f006fa2a16c409d8b91d5c42a639839ad968e
Darshan Naik <rdarshannaik2503@gmail.com>
2026-06-02 19:57:12 +0530

    Implement login API with JWT token generation

--- src/main/java/com/divyasphere/service/AuthService.java
+++ src/main/java/com/divyasphere/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.divyasphere.service;
 
+import com.divyasphere.dto.request.LoginRequest;
 import com.divyasphere.dto.request.SignupRequest;
 import com.divyasphere.dto.response.ApiResponse;
+import com.divyasphere.dto.response.AuthResponse;
 
 public interface AuthService {
 
     ApiResponse<String> signup(SignupRequest request);
+
+    ApiResponse<AuthResponse> login(LoginRequest request);
 }
